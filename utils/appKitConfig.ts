7085bf2d492428c9145190c3b9828b9071e8fece
Decentrastates <102788355+Decentrastates@users.noreply.github.com>
2025-05-09 13:53:13 +0800

    chore: 更新环境变量和项目ID配置
    更新了 `appKitConfig.ts` 和 `deploy.ts` 文件中的环境变量和项目ID配置，确保项目在部署时使用正确的密钥和API端点。这些更改是为了避免在部署过程中出现配置错误。

--- utils/appKitConfig.ts
+++ utils/appKitConfig.ts
@@ -2,9 +2,10 @@ import { cookieStorage, createStorage } from "wagmi";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { mainnet, polygon, arbitrum } from "@reown/appkit/networks";
 import type { AppKitNetwork } from "@reown/appkit/networks";
+import { PUB_WALLET_CONNECT_PROJECT_ID } from "@/constants";
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.PUB_WALLET_CONNECT_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694"; // this is a public projectId only to use on localhost
+export const projectId = PUB_WALLET_CONNECT_PROJECT_ID || "8932e26c2b11da0bc00da23e10bd9294"; // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error("Project ID is not defined");
